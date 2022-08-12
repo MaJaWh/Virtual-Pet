eecef3058f4f4c1843dde16ebad45795b9f11aeb
Mark Whitehead <mjamesw2010@gmail.com>
2022-08-12 23:10:42 +0100

    created a new growUp function on Pet

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -9,4 +9,18 @@ describe("constructor", () => {
     const pet = new Pet("Fido");
     expect(pet.name).toEqual("Fido");
   });
+
+  it("has initial age of 0", () => {
+    const pet = new Pet("Fido");
+    expect(pet.age).toEqual(0);
+  });
+});
+
+describe("growUP", () => {
+  it("increments the age by 1", () => {
+    const pet = new Pet("Fido");
+    pet.growUP();
+
+    expect(pet.age).toEqual(1);
+  });
 });
